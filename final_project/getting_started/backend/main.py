fe9b271875078d83e4fe100c4c1dc3127add67dd
Franck Tiomo <ef.tiomo@gmail.com>
2025-01-17 13:37:54 -0500

    Year filter working

--- final_project/getting_started/backend/main.py
+++ final_project/getting_started/backend/main.py
@@ -17,26 +17,57 @@ app.add_middleware(
 )
 
 @app.get('/api/v1/regular_search')
-async def search(search_query: str, skip: int = 0, limit: int = 10) -> dict:
+async def search(search_query: str, skip: int = 0, limit: int = 10, year: str | None = None) -> dict:
     es = get_es_client(max_retries=5, sleep_time=5)
+    query = {
+        'bool': {
+            'must': [
+                {
+                    'multi_match': {
+                        'query': search_query,
+                        'fields': ['title', 'explanation'],
+                    }
+                }
+            ]
+        }
+    }
+
+    if year:
+        query['bool']['filter'] = [
+            {
+                'range': {
+                    'date': {
+                        'gte': f'{year}-01-01',
+                        'lte': f'{year}-12-31',
+                        'format': 'yyyy-MM-dd'
+                    }
+                }
+            }
+        ]
+
     response = es.search(
         index=INDEX_NAME,
         body={
-            'query': {
-                'multi_match': {
-                    'query': search_query,
-                    'fields': ['title', 'explanation']
-                }
-            },
+            'query': query,
             'from': skip,
             'size': limit
         },
-        filter_path=['hits.hits._source, hits.hits_score']
+        filter_path=['hits.hits._source, hits.hits_score', 'hits.total']
     )
-    hits=response['hits']['hits']
+
+    total_hits = get_total_hits(response)
+    max_pages = calculate_max_pages(total_hits, limit)
+
     return {
-        'hits': hits 
+        'hits': response['hits'].get('hits', []),
+        'max_pages': max_pages, 
     }
+
+def get_total_hits(response: dict) -> int:
+    return response['hits']['total']['value']
+
+def calculate_max_pages(total_hits: int, limit: int) -> int:
+    return (total_hits + limit - 1) // limit
 
 @app.get('/api/v1/get_docs_per_year_count/')
 async def get_docs_per_year_count(search_query: str) -> dict:
